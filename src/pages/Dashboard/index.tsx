98ea227b515aba2dfc1e3537729bfe414474ce10
Italo Sousa <italops94@gmail.com>
2020-10-04 13:49:24 -0300

    eslint config

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,49 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
-  function handleAddRepository() {
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    try {
+      event.preventDefault();
+      const { data } = await api.get(`repos/${newRepo}`);
 
+      setRepositories([...repositories, data]);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <>
       <img src={logoImg} alt="Github explorer" />
       <Title>Explore repositórios no GitHub</Title>
-      <Form>
+      <Form onSubmit={handleAddRepository}>
         <input
           value={newRepo}
-          onChange={event => setNewRepo(event.target.value)}
+          onChange={(event) => setNewRepo(event.target.value)}
           placeholder="digite o nome do repositório"
         />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href='teste'>
-          <img
-            src="https://avatars1.githubusercontent.com/u/11684877?s=460&u=4fa07a5bfb56758dafd3b1b2747dfa4e5ad2e867&v=4"
-            alt='Italo Sousa'
-          />
-          <div>
-            <strong>Github Explorer</strong>
-            <p>Explore git hub repositories all around the internet</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight  size={20}/>
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
 
     </>
   );
-}
+};
 
 export default Dashboard;
