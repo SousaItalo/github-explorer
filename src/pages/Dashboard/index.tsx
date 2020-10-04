bca39282aee460394c99d072e22034b15cee4ab9
Italo Sousa <italops94@gmail.com>
2020-10-04 20:03:22 -0300

    changing link to a tag

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, FormEvent, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -67,7 +66,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+          <a key={repository.full_name} href={`/repositories/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -78,7 +77,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </Link>
+          </a>
         ))}
       </Repositories>
 
